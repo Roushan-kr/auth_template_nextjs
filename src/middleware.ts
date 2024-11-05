d086e358189fb85e7b7bd039ea61026c92484b8d
Roushan <kumar7roushanjha2005@gmail.com>
2024-11-06 03:26:07 +0530

    add verification and reset routes to public paths in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,10 +4,10 @@ import { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname;
-    const publicPaths = ["/login","/signup"];
+    const publicPaths = ["/login","/signup","/auth/verify","/auth/reset" ];
     
     const isPublicPath = publicPaths.includes(path);
-
+    
     const token = request.cookies.get("token")?.value || "";
 
     if(isPublicPath && token){
